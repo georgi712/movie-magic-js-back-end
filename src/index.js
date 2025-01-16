e2635e20236d22dad8b9a14717c2f38fa215b9a7
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-01-16 17:16:48 +0200

    Making home controller and intial templates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import routes from './routes.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('It works!')
-})
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.use('/static', express.static('src/public'));
+app.use(express.urlencoded({extended: false}));
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log('App is running on http://localhost:3000...');
