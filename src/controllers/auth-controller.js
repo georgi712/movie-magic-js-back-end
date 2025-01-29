2aff5ffc5c82f846c437b358fa29d7452cb27d62
georgi712 <152554904+georgi712@users.noreply.github.com>
2025-01-29 18:30:36 +0200

    Register and auth functionality

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import authService from '../services/auth-service.js';
+
+const authController = Router();
+
+authController.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
+authController.post('/register', async (req, res) => {
+    const userData = req.body;
+    authService.register(userData)
+})
+
+export default authController
